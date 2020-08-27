f77a157c1478a441d3a51cf541a8d728d37af46a
Mojtabarezaei4 <54796834+Mojtabarezaei4@users.noreply.github.com>
2020-08-27 12:00:09 +0200

    Klart

---
+++ ForstaUppgiften/ForstaUppgiften/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ForstaUppgiften
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hi, what's your name?");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+            String age = Console.ReadLine();
+
+            Console.WriteLine("What's Your address?");
+            String address = Console.ReadLine();
+
+            Console.WriteLine("What's your post address?");
+            String postAddress = Console.ReadLine();
+
+            Console.WriteLine("Which city are you living in?");
+            String city = Console.ReadLine();
+
+            Console.WriteLine("Please write your telephone number.");
+            String telephoneNumber = Console.ReadLine();
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Information: " + "\n" + "\n" + "Your name: " + "\t" + name
+                                + "\n" + "Your age: " + "\t" + age
+                                + "\n" + "Your address: " + "\t" + address
+                                + "\n" + "Your post address: " + "\t" + postAddress
+                                + "\n" + "Your city: " + "\t" + city
+                                + "\n" + "Your telephonenumber: " + "\t" + telephoneNumber
+            );
+        }
+    }
+}
--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace HelloWorld
 {
@@ -6,7 +7,45 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Niklas");
+            Console.WriteLine("Hi, what's your name?");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+            String age = Console.ReadLine();
+
+            Console.WriteLine("Are you alive? (Choose a number)" + "\n" + "1. Yes" + "\n" + "2. No");
+            int aliveInt = Convert.ToInt32(Console.ReadLine());
+
+            bool alive = true;
+
+            if (aliveInt == 1)
+            {
+                alive = true;
+
+            }
+            else
+            {
+                alive = false;
+            }
+
+            Console.WriteLine("Are you good at math?" + "\n" + " 2 + 2 = ?");
+            int answer = Convert.ToInt32(Console.ReadLine());
+
+
+            var num1 = 2;
+            var num2 = 2;
+            var sum = num1 + num2;
+
+
+            
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Information: " + "\n" + "\n" + "Your name: " + "\t" + name
+                                + "\n" + "Your age: " + "\t" + age
+                                + "\n" + "You are alive: " + "\t" + alive
+                                + "\n" + "You are not: " + "\t" +  "2 + 2 = " + sum
+            );
         }
     }
 }
