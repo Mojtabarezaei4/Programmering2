4efe17e3218f2727b915e2a0960f9e07aedb244d
Mojtabarezaei4 <54796834+Mojtabarezaei4@users.noreply.github.com>
2020-09-02 17:47:37 +0200

    Uppgift 2 är klar nu!

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,42 +10,60 @@ namespace HelloWorld
             Console.WriteLine("Hi, what's your name?");
             String name = Console.ReadLine();
 
-            Console.WriteLine("How old are you?");
-            String age = Console.ReadLine();
+            bool getValue = true;
 
-            Console.WriteLine("Are you alive? (Choose a number)" + "\n" + "1. Yes" + "\n" + "2. No");
-            int aliveInt = Convert.ToInt32(Console.ReadLine());
+            while (getValue)
+            {
+                if (name != ""){
+                    getValue = false;
+                }
+                else
+                {
+                    Console.WriteLine("Please write your name!");
+                    name = Console.ReadLine();
+                }
+            }
+            
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
 
             bool alive = true;
 
-            if (aliveInt == 1)
-            {
-                alive = true;
+            Console.WriteLine("Are you alive? (Choose a number)" + "\n" + "1. Yes" + "\n" + "2. No");
+            var aliveInt = Convert.ToInt32(Console.ReadLine());
 
-            }
-            else
+            switch(aliveInt)
             {
-                alive = false;
+                case 1:
+                        alive = true;
+                        break;
+                case 2: 
+                        alive = false;
+                        break;
             }
 
             Console.WriteLine("Are you good at math?" + "\n" + " 2 + 2 = ?");
             int answer = Convert.ToInt32(Console.ReadLine());
 
-
             var num1 = 2;
             var num2 = 2;
             var sum = num1 + num2;
 
+            Console.WriteLine("");
 
-            
+            for(int i = 0; age >= i; i++)
+            {
+                Console.WriteLine("<3, ");
+            }
 
             Console.WriteLine("");
 
             Console.WriteLine("Information: " + "\n" + "\n" + "Your name: " + "\t" + name
                                 + "\n" + "Your age: " + "\t" + age
                                 + "\n" + "You are alive: " + "\t" + alive
-                                + "\n" + "You are not: " + "\t" +  "2 + 2 = " + sum
-            );
+                                + "\n" + "You are not: " + "\t" +  "2 + 2 = " + sum);
+
+
         }
     }
 }
