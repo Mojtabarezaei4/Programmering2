dac07854f12e2a0d3d0c96342ec5f75bf0d901b7
Mojtabarezaei4 <54796834+Mojtabarezaei4@users.noreply.github.com>
2020-09-03 13:54:04 +0200

    Array exercise

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
 
 namespace HelloWorld
 {
@@ -7,62 +6,30 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi, what's your name?");
-            String name = Console.ReadLine();
+            var informations = new string[5];
 
-            bool getValue = true;
+            var ages = new string[5];
 
-            while (getValue)
+            var liveStatus = new string[5];
+
+            for (int i = 0; i < informations.Length; i++)
             {
-                if (name != ""){
-                    getValue = false;
-                }
-                else
-                {
-                    Console.WriteLine("Please write your name!");
-                    name = Console.ReadLine();
-                }
-            }
+                Console.WriteLine("Hi, what's your name?");
+                informations[i] = Console.ReadLine();
             
-            Console.WriteLine("How old are you?");
-            int age = Convert.ToInt32(Console.ReadLine());
-
-            bool alive = true;
-
-            Console.WriteLine("Are you alive? (Choose a number)" + "\n" + "1. Yes" + "\n" + "2. No");
-            var aliveInt = Convert.ToInt32(Console.ReadLine());
-
-            switch(aliveInt)
-            {
-                case 1:
-                        alive = true;
-                        break;
-                case 2: 
-                        alive = false;
-                        break;
+                Console.WriteLine("How old are you?");
+                ages[i] = Console.ReadLine();
+           
+                Console.WriteLine("Are you alive?");
+                liveStatus[i] = Console.ReadLine();
             }
 
-            Console.WriteLine("Are you good at math?" + "\n" + " 2 + 2 = ?");
-            int answer = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
 
-            var num1 = 2;
-            var num2 = 2;
-            var sum = num1 + num2;
-
-            Console.WriteLine("");
-
-            for(int i = 0; age >= i; i++)
+            for (int i = 4; i >= 0; i--)
             {
-                Console.WriteLine("<3, ");
+                Console.WriteLine(informations[i] + " is " + ages[i] + " old and he is alive? " + liveStatus[i]);
             }
-
-            Console.WriteLine("");
-
-            Console.WriteLine("Information: " + "\n" + "\n" + "Your name: " + "\t" + name
-                                + "\n" + "Your age: " + "\t" + age
-                                + "\n" + "You are alive: " + "\t" + alive
-                                + "\n" + "You are not: " + "\t" +  "2 + 2 = " + sum);
-
 
         }
     }
