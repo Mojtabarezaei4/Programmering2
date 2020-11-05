c379baff984f5c9928e4618541f2a42301e0c75d
Mojtabarezaei4 <54796834+Mojtabarezaei4@users.noreply.github.com>
2020-11-05 11:35:58 +0100

    Number-buttons are working now!

---
+++ Calculator/Calculator/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(sender is Button button)
+            {
+                switch (button.Content)
+                {
+                    case "0":
+                    case "1":
+                    case "2":
+                    case "3":
+                    case "4":
+                    case "5":
+                    case "6":
+                    case "7":
+                    case "8":
+                    case "9":
+                        TextFied.Text += button.Content;
+                        break;
+                }
+            }
+        }
+
+        private void Button_Click_CE(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_C(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_BackSpace(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_Division(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_Multiplication(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_Minus(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_Plus(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_Equal(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_Plus_Minus(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_Dot(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+
+
+}
